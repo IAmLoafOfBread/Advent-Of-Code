f1f8fcdd9d2d24d1ea38171425b6b1d691183651
Bead <WholeWheatLoafBread@gmail.com>
2022-04-26 09:02:46 +0800

    Scopped some variables
    0 initialized some variables
    
    Changed makefile to use C++20
    
    Removed executable result from repository

--- Day 1/Main.cpp
+++ Day 1/Main.cpp
@@ -8,21 +8,22 @@
 template<typename t>
 void parse_inputFile(const std::string& filePath, std::vector<t>& vectorOutput){
   
-  std::ifstream InputFile(filePath);
-  std::string StringBuffer;
-  std::uint32_t IntBuffer;
-  for(std::uint32_t i = 0; std::getline(InputFile, StringBuffer); i++){
-    std::uint32_t NullIndex = 0;
-    for(char& c : StringBuffer){
-      NullIndex++;
-    }
-    uint32_t Divisor = 1;
-    IntBuffer = 0;
-    for(std::uint32_t j = NullIndex; j != 0; j--){
-      IntBuffer += (StringBuffer[j - 1] - 48) * Divisor;
-      Divisor *= 10;
-    }
-    vectorOutput.push_back(IntBuffer);
+  {
+      std::ifstream InputFile(filePath);
+      std::string StringBuffer = {0};
+      for(std::uint32_t i = 0; std::getline(InputFile, StringBuffer); i++){
+        std::uint32_t NullIndex = 0;
+        for(char& c : StringBuffer){
+          NullIndex++;
+        }
+        std::uint32_t IntBuffer = 0;
+        std::uint32_t Divisor = 1;
+        for(std::uint32_t j = NullIndex; j != 0; j--){
+          IntBuffer += (StringBuffer[j - 1] - 48) * Divisor;
+          Divisor *= 10;
+        }
+        vectorOutput.push_back(IntBuffer);
+      }
   }
   
   return;
@@ -31,8 +32,9 @@ void parse_inputFile(const std::string& filePath, std::vector<t>& vectorOutput){
 
 int main(void){
   
-  std::vector<uint32_t> ReportInputs;
+  std::vector<uint32_t> ReportInputs(0, 0);
   parse_inputFile("Input.txt", ReportInputs);
+  std::uint32_t IncreasedCount = 0;
 
 /* --------------TESTING PARSE----------------*/
 //  for(uint32_t v : ReportInputs){
