c34f9a556643bf276796e1c1c616ede8798c4467
Bead <WholeWheatLoafBread@gmail.com>
2022-04-27 11:04:39 +0800

    Day 3, part 1 COMPLETE

--- Day 3/Main.cpp
+++ Day 3/Main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cmath>
 
 struct byte_st{
   std::vector<bool> bits;
@@ -28,10 +29,51 @@ void parse_inputFile(const std::string& filePath, std::vector<struct byte_st>& v
   
 }
 
+std::int32_t convert_binary(const byte_st& byte){
+
+  std::int32_t Result = 0;
+  std::uint32_t IndexIncrement = 0;
+  for(std::uint32_t i = byte.bits.size(); i > 0; i--){
+    Result += byte.bits[IndexIncrement] * std::pow(2, i - 1);
+    IndexIncrement++;
+  }
+
+  return Result;
+
+}
+
+std::uint32_t find_powerConsumption(const std::vector<byte_st> vectorInput){
+
+  struct byte_st GammaRate;
+  struct byte_st EpsilonRate;
+  for(std::uint32_t i = 0; i < vectorInput[0].bits.size(); i++){
+    std::uint32_t FalseCount = 0;
+    std::uint32_t TrueCount = 0;
+    for(std::uint32_t j = 0; j < vectorInput.size(); j++){
+      if(vectorInput[j].bits[i] == 0){
+        FalseCount++;
+      }else{
+        TrueCount++;
+      }
+    }
+    if(FalseCount > TrueCount){
+      GammaRate.bits.push_back(false);
+      EpsilonRate.bits.push_back(true);
+    }else if(FalseCount < TrueCount){
+      GammaRate.bits.push_back(true);
+      EpsilonRate.bits.push_back(false);
+    }
+  }
+
+  return convert_binary(GammaRate) * convert_binary(EpsilonRate);
+
+}
+
 int main(void){
 
   std::vector<byte_st> ReportInputs;
   parse_inputFile("Input.txt", ReportInputs);
+  std::cout << find_powerConsumption(ReportInputs) << std::endl;
 
 /* --------------TESTING PARSE----------------*/
 //  for(byte_st byte : ReportInputs){
