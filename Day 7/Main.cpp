9d41f93324cef38f8a88ddd6be2e47f47ecd0880
Bead <WholeWheatLoafBread@gmail.com>
2022-05-27 10:34:52 +0800

    Day 7, part 1 COMPLETE

--- Day 7/Main.cpp
+++ Day 7/Main.cpp
@@ -6,12 +6,31 @@
 #include <fstream>
 #include <cmath>
 
+struct sortelem_st{
+  std::uint32_t value;
+  std::uint32_t count;
+};
+
 void parse_inputFile(const std::string& filePath, std::vector<std::uint32_t>& vectorOutput){
 
   std::ifstream InputFile(filePath);
 
   std::string StringBuffer = {0};
-  
+  std::getline(InputFile, StringBuffer);
+  for(std::uint32_t i = 0; i < StringBuffer.size(); i++){
+    std::uint32_t DigitCount = 0;
+    while((StringBuffer[i + DigitCount] != ',') && (StringBuffer[i + DigitCount] != '\0')){
+      DigitCount++;
+    }
+    std::uint32_t IntBuffer = 0;
+    std::uint32_t Multiplier = 1;
+    for(std::uint32_t j = DigitCount; j != 0; j--){
+      IntBuffer += (StringBuffer[i + (j - 1)] - 48) * Multiplier;
+      Multiplier *= 10;
+    }
+    vectorOutput.push_back(IntBuffer);
+    i += DigitCount;
+  }
 
   InputFile.close();
 
@@ -19,10 +38,128 @@ void parse_inputFile(const std::string& filePath, std::vector<std::uint32_t>& ve
 
 }
 
+void set_sort(const std::vector<std::uint32_t>& inputVector, std::vector<struct sortelem_st>& outputVector){
+
+  std::uint32_t Index = 0;
+LABEL_LOOPSTART:
+  if(Index < inputVector.size()){
+    for(auto& n : outputVector){
+      if(n.value == inputVector[Index]){
+        n.count++;
+        Index++;
+        goto LABEL_LOOPSTART;
+      }
+    }
+    outputVector.push_back({inputVector[Index], 1});
+    Index++;
+    goto LABEL_LOOPSTART;
+  }
+
+  return;
+
+}
+
+float calculate_average(const std::vector<std::uint32_t>& inputVector){
+
+  float Result = 0;
+  for(auto v : inputVector){
+    Result += v;
+  }
+
+  return Result / inputVector.size();
+
+}
+
+std::vector<std::uint32_t> calculate_medians(const std::vector<std::uint32_t>& inputVector){
+
+  std::vector<std::uint32_t> SortVector = inputVector;
+  bool Complete = false;
+  while(!(Complete)){
+    for(std::uint32_t i = 0; i < SortVector.size(); i++){
+      for(std::uint32_t j = i == 0 ? 1 : 0; j < SortVector.size() - (i + 1 == SortVector.size() ? 1 : 0); j += j + 1 == i ? 2 : 1){
+        if(SortVector[j] > SortVector[i]){
+          if(j == 0 || (SortVector[j - 1] <= SortVector[i])){
+            for(std::uint32_t k = i; k > j; k--){
+              std::uint32_t IntBuffer = SortVector[k - 1];
+              SortVector[k - 1] = SortVector[k];
+              SortVector[k] = IntBuffer;
+            }
+            break;
+          }
+          std::uint32_t IntBuffer = SortVector[j - 1];
+          SortVector[j - 1] = SortVector[i];
+          SortVector[i] = IntBuffer;
+          break;
+        }
+      }
+    }
+    for(std::uint32_t i = 1; i < SortVector.size() - 1; i+= i + 3 >= SortVector.size() ? 1 : 2){
+      if(!(SortVector[i - 1] <= SortVector[i]) || !(SortVector[i + 1] >= SortVector[i])){
+        Complete = false;
+        break;
+      }else if((SortVector[i - 1] <= SortVector[i]) && (SortVector[i + 1] >= SortVector[i])){
+        Complete = true;
+      }
+    }
+  }
+  std::uint32_t Middle = SortVector.size() / 2;
+  bool Plural = Middle == (float)SortVector.size() / 2 ? true : false;
+  if(Plural){
+
+    return {SortVector[Middle - 1], SortVector[Middle]};
+
+  }
+
+  return {SortVector[Middle]};
+
+}
+
+std::vector<std::uint32_t> calculate_modes(const std::vector<std::uint32_t>& inputVector){
+
+  std::vector<struct sortelem_st> NodesBuffer;
+  set_sort(inputVector, NodesBuffer);
+  std::uint32_t PeakCount = 0;
+  for(std::uint32_t i = 0; i < NodesBuffer.size(); i++){
+    PeakCount = NodesBuffer[i].count > PeakCount ? NodesBuffer[i].count : PeakCount;
+  }
+  std::vector<std::uint32_t> ModeValues;
+  for(std::uint32_t i = 0; i < NodesBuffer.size(); i++){
+    if(NodesBuffer[i].count == PeakCount){
+      ModeValues.push_back(NodesBuffer[i].value);
+    }
+  }
+
+  return ModeValues;
+
+}
+
+std::uint32_t calculate_alignment(const std::vector<std::uint32_t>& inputVector, const std::uint32_t& alignValue){
+
+  std::uint32_t FuelCost = 0;
+  for(const auto& v : inputVector){
+    if(v > alignValue){
+      FuelCost += v - alignValue;
+    }else if(v < alignValue){
+      FuelCost += alignValue - v;
+    }
+  }
+
+  return FuelCost;
+
+}
+
 int main(void){
 
   std::vector<std::uint32_t> ReportInputs;
   parse_inputFile("Input.txt", ReportInputs);
+  std::cout << "AVERAGE: " << calculate_average(ReportInputs) << std::endl;
+  for(auto v : calculate_medians(ReportInputs)){
+    std::cout << "MEDIANS: " << v << std::endl;
+  }
+  for(auto v : calculate_modes(ReportInputs)){
+  std::cout << "MODES: " << v << std::endl;
+  }
+  std::cout << calculate_alignment(ReportInputs, 336) << std::endl;
 
 /* --------------TESTING PARSE----------------*/
 //  for(auto i : ReportInputs){
