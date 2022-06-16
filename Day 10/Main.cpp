31f98d099f9bffcfc4accefcbe48e1ea3f20e3eb
Bead <WholeWheatLoafBread@gmail.com>
2022-06-16 15:52:21 +0800

    Finished Day 10, part 1

--- Day 10/Main.cpp
+++ Day 10/Main.cpp
@@ -29,7 +29,6 @@ struct chunkunit_st{
   char connectType;
   struct chunkunit_st* connection;
   std::uint32_t index;
-  bool searched;
 };
 
   std::vector<std::vector<chunkunit_st>> ChunkLines;
@@ -41,7 +40,7 @@ struct chunkunit_st{
       if(stringed[i] == '(' || stringed[i] == ')'){
         ConnectTypeBuffer = WelcomeBuffer ? stringed[i] + 1 : stringed[i] - 1;
       }
-      LineBuffer.push_back({stringed[i], WelcomeBuffer, ConnectTypeBuffer, nullptr, i, false});
+      LineBuffer.push_back({stringed[i], WelcomeBuffer, ConnectTypeBuffer, nullptr, i});
     }
     ChunkLines.push_back(LineBuffer);
   }
@@ -52,57 +51,32 @@ struct chunkunit_st{
   const std::uint32_t ANGLE_BRACKET_SCORE = 25137;
   std::uint32_t Result = 0;
   for(auto& line : ChunkLines){
+    std::vector<std::uint32_t> ErrorIndices;
     for(std::uint32_t i = 0; i < line.size(); i++){
-      std::uint32_t TypeCounter = 1;
-      if(line[i].searched == false && line[i].welcome == true){
+      std::uint32_t WelcomeCounter = 1;
+      if(line[i].welcome == true){
         for(std::uint32_t j = i + 1; j < line.size(); j++){
-          TypeCounter += line[j].type == line[i].type ? 1 : 0;
-          TypeCounter -= line[j].type == line[i].connectType ? 1 : 0;
-          if(TypeCounter == 0){
+          WelcomeCounter += line[j].welcome == true ? 1 : 0;
+          WelcomeCounter -= line[j].welcome == false ? 1 : 0;
+          if(WelcomeCounter == 0){
             line[i].connection = &line[j];
             line[j].connection = &line[i];
-            line[i].searched = true;
-            line[j].searched = true;
+            if(line[i].connectType != line[j].type){
+              ErrorIndices.push_back(j);
+            }
             break;
           }
         }
       }
-      line[i].searched = true;
     }
-  }
-  for(auto& line : ChunkLines){
-    for(std::uint32_t i = 0; i < line.size(); i++){
-      if(line[i].connection == nullptr){
-        std::cout << i << ' ' << "NULL" << std::endl;
-      }else{
-        std::cout << i << ' ' << line[i].connection->index << std::endl;
-      }
-      bool ErrorFound = false;
-      if(line[i].connection != nullptr && line[i].welcome == true){
-        for(std::uint32_t j = line[i].connection->index - 1; j > i; j--){
-          if(line[j].connection == nullptr){
-            Result += line[i].type == '(' ? PARENTHESIS_SCORE : 0;
-            Result += line[i].type == '[' ? SQUARE_BRACKET_SCORE : 0;
-            Result += line[i].type == '{' ? CURLY_BRACKET_SCORE : 0;
-            Result += line[i].type == '<' ? ANGLE_BRACKET_SCORE : 0;
-            ErrorFound = true;
-            break;
-          }
-          if(line[j].connection->index > line[i].connection->index){
-            Result += line[j].type == ')' ? PARENTHESIS_SCORE : 0;
-            Result += line[j].type == ']' ? SQUARE_BRACKET_SCORE : 0;
-            Result += line[j].type == '}' ? CURLY_BRACKET_SCORE : 0;
-            Result += line[j].type == '>' ? ANGLE_BRACKET_SCORE : 0;
-            ErrorFound = true;
-            break;
-          }
-        }
-      }
-      ErrorFound = line[i].connection == nullptr && line[i].welcome == false ? true : ErrorFound;
-      if(ErrorFound){
-        break;
-      }
+    std::uint32_t IndexBuffer = line.size();
+    for(std::uint32_t i = 0; i < ErrorIndices.size(); i++){
+      IndexBuffer = IndexBuffer > ErrorIndices[i] ? ErrorIndices[i] : IndexBuffer;
     }
+    Result += line[IndexBuffer].type == ')' ? PARENTHESIS_SCORE : 0;
+    Result += line[IndexBuffer].type == ']' ? SQUARE_BRACKET_SCORE : 0;
+    Result += line[IndexBuffer].type == '}' ? CURLY_BRACKET_SCORE : 0;
+    Result += line[IndexBuffer].type == '>' ? ANGLE_BRACKET_SCORE : 0;
   }
 
   return Result;
